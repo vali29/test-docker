f2f9f47e1bcbb5f24dd8c7bdeaa273775344ce2f
root <root@test-unix.cos.agilent.com>
2017-10-10 09:41:27 -0600

    Added Template resource for HTTP

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,26 +1,7 @@
 package 'httpd'
 package 'tree'
 
-file '/var/www/html/index.html' do
-        content " <h1> This is test Website </h1> "
-end
-
-service 'httpd' do
-        action [ :enable, :start ]
-
-end
-
-file '/etc/motd' do
-        content  " Property of Mastan \n
-
-        IPADDRESS: #{node['ipaddress']}
-        HOSTNAME: #{node['hostname']}
-        INMEMORY: #{node['memory'] ['total']}
-        CPU: #{node['cpu'] ['0'] ['mhz']}
-        \n "
-
-        mode '0644'
-        owner 'root'
-        group 'root'
+template '/var/www/html/index.html' do
+        source 'index.html.erb'
 end
 
